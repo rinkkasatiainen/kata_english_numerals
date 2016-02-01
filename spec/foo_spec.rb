3cf75b6d30b21b3a355d34595da2a3436c8232e5
Aki Salmi <aki.salmi@iki.fi>
2016-02-01 19:39:35 +0200

    Numerate numbers up to 20 and multiples of ten up to 100.

---
+++ spec/foo_spec.rb
@@ -0,0 +1,67 @@
+require_relative 'spec_helper'
+
+
+class EnglishNumerator
+  def of ( arabicToEnglish )
+    # TODO make class with constants of languae only EnglishNumbers
+    least_siginificant_by_number = {
+        1 => "one",
+        2 => "two",
+        3 => "three",
+        4 => "four",
+        # 5 => "two",
+        # 6 => "two",
+        # 7 => "two",
+        # 8 => "two",
+        # 9 => "two",
+        # 10 => "two",
+        # 11 => "two",
+        # 12 => "two",
+        # 13 => "two",
+    }
+    powers_of_ten_by_number = {
+        0 => '',
+        5 => "fifty",
+    }
+    if arabicToEnglish % 10 == 0
+      return powers_of_ten_by_number[arabicToEnglish / 10]
+    end
+
+    if arabicToEnglish > 13
+      return least_siginificant_by_number[arabicToEnglish - 10] + "teen"
+    end
+    least_siginificant_by_number[arabicToEnglish]
+  end
+
+end
+
+describe EnglishNumerator do
+
+  numertor = EnglishNumerator.new
+
+  context 'for numbers less than 14' do
+    it 'e.g. 1' do
+      expect(numertor.of(1)).to eq "one"
+    end
+
+    #we need one for 13
+  end
+
+  context 'for numbers less than 20' do
+    it 'e.g. 14' do
+      expect(numertor.of(14)).to eq "fourteen"
+    end
+  end
+
+  context 'for numbers less than 100' do
+    it 'dividable by ten, e.g. 50' do
+      expect( numertor.of(50)).to eq "fifty"
+    end
+    it 'not dividable by ten, e.g. 54' do
+      expect( numertor.of(54)).to eq "fiftyfour"
+    end
+  end
+
+
+
+end
